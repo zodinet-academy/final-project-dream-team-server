fd88050bd3b50b3597b944613305bf55782a1ed2
TrucNguyennn <truc.nguyen@zodinet.tech>
2022-08-01 13:08:16 +0700

    feat: setup swagger and config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,26 @@
 import { Logger, Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { UsersModule } from "./modules/users/users.module";
+import * as Joi from "@hapi/joi";
+import { AutomapperModule } from "@automapper/nestjs";
+import { classes } from "@automapper/classes";
 
 @Module({
   providers: [Logger],
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `env/.env.${process.env.NODE_ENV || "local"}`, // .env.development
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid("local", "development", "production")
+          .default("local"),
+      }),
+    }),
+    AutomapperModule.forRoot({
+      strategyInitializer: classes(),
+    }),
+  ],
 })
 export class AppModule {}
