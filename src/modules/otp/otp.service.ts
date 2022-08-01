8673d630dbe9318744d2673f7c4dc23e058e7629
MinhTuanVo1 <tuan.vo@zodinet.tech>
2022-08-01 10:31:30 +0700

    feat: init otp function

---
+++ src/modules/otp/otp.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@nestjs/common";
+import { Twilio } from "twilio";
+
+@Injectable()
+export class OtpService {
+  private twilioClient: Twilio;
+  constructor(private readonly configService: ConfigService) {
+    const accountSid = configService.get("TWILIO_ACCOUNT_SID");
+    const authToken = configService.get("TWILIO_AUTH_TOKEN");
+
+    this.twilioClient = new Twilio(accountSid, authToken);
+  }
+}
